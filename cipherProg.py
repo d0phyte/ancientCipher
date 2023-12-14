3af654a7fab4c23198fc0f8c63de180cf19b2695
JTcgh <74654976+d0phyte@users.noreply.github.com>
2023-12-14 17:13:56 +0000

    Copied from the other Repo, keepGoing variable keeps the program running until the user decides to exit.

--- cipherProg.py
+++ cipherProg.py
@@ -1,44 +1,66 @@
-#testing
-
 alphabet = [chr(i) for i in range(97, 123)]
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+keepGoing = True
+while keepGoing:
 
-def encrypt(text, shift):
-    cipherText = ""
-    for letter in text:
-        if letter in alphabet:
-            position = alphabet.index(letter)
-            newPosition = (position + shift) % 26
-            newLetter = alphabet[newPosition]
-            cipherText += newLetter
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+
+
+    def encrypt(text, shift):
+        cipherText = ""
+        for letter in text:
+            if letter in alphabet:
+                position = alphabet.index(letter)
+                newPosition = (position + shift) % 26
+                newLetter = alphabet[newPosition]
+                cipherText += newLetter
+            else:
+                cipherText += letter
+        return cipherText
+
+    def decrypt(text, shift):
+        plainText = ""
+        for letter in text:
+            if letter in alphabet:
+                position = alphabet.index(letter)
+                newPosition = (position - shift) % 26
+                newLetter = alphabet[newPosition]
+                plainText += newLetter
+            else:
+                plainText += letter
+        return plainText
+
+    if direction == "encode":
+        encrypted_text = encrypt(text, shift)
+        print("Encrypted message:" + encrypted_text)
+        userContinue = input("Do you want to keep going?y/n ").lower()
+
+        if userContinue == "y":
+            continue
         else:
-            cipherText += letter
-    return cipherText
+            keepGoing = False
 
-def decrypt(text, shift):
-    plainText = ""
-    for letter in text:
-        if letter in alphabet:
-            position = alphabet.index(letter)
-            newPosition = (position - shift) % 26
-            newLetter = alphabet[newPosition]
-            plainText += newLetter
+
+    elif direction == "decode":
+        decrypted_text = decrypt(text, shift)
+        print("Decrypted message:" + decrypted_text)
+        userContinue = input("Do you want to keep going?y/n ").lower()
+
+        if userContinue == "y":
+            continue
         else:
-            plainText += letter
-    return plainText
+            keepGoing = False
+    else:
+        
+        userContinue = input("Invalid direction. Do you want to keep going?y/n ").lower()
 
-if direction == "encode":
-    encrypted_text = encrypt(text, shift)
-    print("Encrypted message:" + encrypted_text)
-elif direction == "decode":
-    decrypted_text = decrypt(text, shift)
-    print("Decrypted message:" + decrypted_text)
-else:
-    print("Invalid direction. Please try again.")
-
+        if userContinue == "y":
+            continue
+        else:
+            keepGoing = False
 
 
 
